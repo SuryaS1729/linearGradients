db89e74670c9a504efa8d1a8f46551d55ecb0563
SuryaS1729 <surya.curiousfusion@gmail.com>
2025-03-11 20:24:01 +0530

    added grid layout to gradients

--- app/index.tsx
+++ app/index.tsx
@@ -1,23 +1,48 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 import { LinearGradient } from 'expo-linear-gradient';
+import { StatusBar } from 'expo-status-bar';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { ScrollView } from 'react-native-gesture-handler';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import { gradients } from '../gradients';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <LinearGradient
-        // Background Linear Gradient
-        colors={['rgba(0,0,0,0.8)', 'transparent']}
-        style={styles.background}
-      />
-      <LinearGradient
-        // Button Linear Gradient
-        colors={['#4c669f', '#3b5998', '#192f6a']}
-        style={styles.button}>
-        <Text style={styles.text}>Sign in with Facebook</Text>
-      </LinearGradient>
-    </View>
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <SafeAreaView style={{ flex: 1 }}>
+        <StatusBar style="light" />
+        <ScrollView>
+          <View style={{ flex: 1, padding: 10 }}>
+            {gradients.map((gradientGroup, index) => (
+              <View key={index} style={{ flexDirection: 'row', justifyContent: 'space-around', marginBottom: 10 }}>
+                {gradientGroup.map((gradient, gradientIndex) => (
+                  <TouchableOpacity key={gradientIndex} style={{ flex: 1, marginVertical: 3, marginHorizontal: 5 }}>
+                    <LinearGradient
+                      colors={gradient.colors}
+                      style={{
+                        padding: 0,
+                        borderWidth: 1,
+                        borderColor: 'red',
+                        borderRadius: 7,
+                        alignItems: 'center',
+                        justifyContent: 'flex-start',
+                        height: 100,
+                      }}
+                    >
+                      <Text style={{ color: 'white', fontWeight: 'bold'}}>{gradient.name}</Text>
+                      <Text style={{ color: 'white', fontWeight: 'bold'}}>{gradient.name}</Text>
+                    </LinearGradient>
+                  </TouchableOpacity>
+                ))}
+              </View>
+            ))}
+          </View>
+        </ScrollView>
+      </SafeAreaView>
+    </GestureHandlerRootView>
   );
 }
+
 
 const styles = StyleSheet.create({
   container: {
